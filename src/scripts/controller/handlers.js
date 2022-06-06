de048d237b60d52b789834f79290ee99d6085ab6
AkbarHusein <muhakbarhusein5@gmail.com>
2022-06-06 23:26:40 +0700

    added page property on handler.js

--- src/scripts/controller/handlers.js
+++ src/scripts/controller/handlers.js
@@ -2,21 +2,25 @@ const Handler = {
   homePage: async (req, res) => {
     res.render('home', {
       layout: '../layout/main-layout',
+      page: 'home',
     });
   },
   forumPage: (req, res) => {
     res.render('forum', {
       layout: '../layout/main-layout',
+      page: 'forum',
     });
   },
   loginPage: (req, res) => {
     res.render('login', {
       layout: '../layout/main-layout',
+      page: 'login',
     });
   },
   signinPage: (req, res) => {
     res.render('signin', {
       layout: '../layout/main-layout',
+      page: 'signin',
     });
   },
 };
